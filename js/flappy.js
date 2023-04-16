10f65bf102e917d397f9c4c393e55aec2788b2e6
Iury Hirabara <dev.iuryhirabara@gmail.com>
2023-04-16 00:01:52 -0300

    Adicionando tubos do chão

--- js/flappy.js
+++ js/flappy.js
@@ -1,34 +1,62 @@
 let wmFlappy = document.querySelector('[wm-flappy]')
-let completeTube = document.querySelector('.complete-tube')
-let maxWidth = (wmFlappy.clientWidth * 1.13).toFixed(0)
-let incrementoMovimento = Number((0.002 * maxWidth).toFixed(0))
+const larguraMaxima = (wmFlappy.clientWidth * 1.13).toFixed(0)
+const incrementoMovimento = Number((0.002 * larguraMaxima).toFixed(0))
 
-wmFlappy.addEventListener('click', () => {
+const limparIntervalo = intervalo => clearInterval(intervalo)
+const gerarTamanho = () => Math.floor(Math.random() * 61) + 30
+
+const criarTubo = tipoTubo => {
+    let tuboCompleto = document.createElement('div')
+    tuboCompleto.classList.add(tipoTubo)
+    
+    let tube = document.createElement('div')
+    tube.classList.add('tube')
+    const tamanho = gerarTamanho()
+    tube.style.padding = `${tamanho}% 46%`
+    tuboCompleto.appendChild(tube)
+
+    let hat = document.createElement('div')
+    hat.classList.add('hat')
+    tuboCompleto.appendChild(hat)
+
+    wmFlappy.appendChild(tuboCompleto)
+    return tuboCompleto
+}
+
+const realizarMovimento = tuboCompleto => {
     let movimento = 0
+
     const intervalo = setInterval(() => {
-        if (movimento >= maxWidth) {
+        if (movimento >= larguraMaxima) {
             console.log(movimento)
             limparIntervalo(intervalo)
             return
         }
         movimento = movimento + incrementoMovimento
-        completeTube.style.right = `${movimento}px`
+        tuboCompleto.style.right = `${movimento}px`
     }, 30)
+}
 
-    const limparIntervalo = (interval) => clearInterval(interval)
+let jogoIniciado = false
+document.addEventListener('keypress', evento => {
+    if (evento.code === 'Enter' && !jogoIniciado) {
+        jogoIniciado = true
+        let tuboCompleto = criarTubo('complete-tube-top')
+        realizarMovimento(tuboCompleto)
+        setInterval(() => {
+            let tuboCompleto = criarTubo('complete-tube-top')
+            realizarMovimento(tuboCompleto)
+        }, 9000)
+
+        setTimeout(() => {
+            let tuboCompleto = criarTubo('complete-tube-bottom')
+            realizarMovimento(tuboCompleto)
+            setInterval(() => {
+                let tuboCompleto = criarTubo('complete-tube-bottom')
+                realizarMovimento(tuboCompleto)
+            }, 9020)
+        }, 4500)
+    } else if (evento.code === 'Space' && jogoIniciado) {
+        console.log('pula')
+    }
 })
-
-// wmFlappy.addEventListener('click', () => {
-//     let maxHeight = wmFlappy.clientHeight
-//     let maxWidth = wmFlappy.clientWidth
-//     setInterval(() => {
-//         let completeTube = document.createElement('div')
-//         let tube = document.createElement('div')
-//         tube.classList.add('tube')
-//         let hat = document.createElement('div')
-//         hat.classList.add('hat')
-//         completeTube.appendChild(tube)
-//         completeTube.appendChild(hat)
-//         wmFlappy.appendChild(completeTube)
-//     }, 1000)
-// })
